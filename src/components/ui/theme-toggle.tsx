d908569d0bd7937cd78e4762a37e13f0c580be40
Emad <emadmohagheghi1383@gmail.com>
2025-08-14 12:32:26 +0330

    theme toggler

---
+++ src/components/ui/theme-toggle.tsx
@@ -0,0 +1,36 @@
+'use client';
+import * as React from 'react';
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import { Button } from '@/components/ui/button';
+
+export function ModeToggle() {
+  const { setTheme, theme, systemTheme } = useTheme();
+
+  return (
+    <Button
+      className="size-8 rounded-full"
+      onClick={() => {
+        {
+          theme === 'dark'
+            ? setTheme('light')
+            : theme === 'light'
+            ? setTheme('dark')
+            : systemTheme === 'dark'
+            ? setTheme('light')
+            : setTheme('dark');
+        }
+      }}
+    >
+      {theme === 'dark' ? (
+        <Sun />
+      ) : theme === 'light' ? (
+        <Moon />
+      ) : systemTheme === 'dark' ? (
+        <Sun />
+      ) : (
+        <Moon />
+      )}
+    </Button>
+  );
+}
